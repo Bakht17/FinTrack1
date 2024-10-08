69df99a9fd71e7026403cb90a951018cce93075e
Bakht17 <61829647+Bakht17@users.noreply.github.com>
2024-10-09 00:06:28 +0300

    Update transactionmanagertest.cpp

--- tests/transactionmanagertest.cpp
+++ tests/transactionmanagertest.cpp
@@ -1,1 +1,8 @@
+#include <gtest/gtest.h>
+#include "transactionmanager.h"
 
+TEST(TransactionManagerTest, AddTransaction) {
+    TransactionManager manager;
+    manager.addTransaction(100.0, "Income");
+    EXPECT_EQ(manager.getTotalIncome(), 100.0);
+}
